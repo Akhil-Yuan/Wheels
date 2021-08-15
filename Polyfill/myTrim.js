061d58683940c6b625e0f4300ba9688a1d6e1956
aki-yuan <yxqi09@126.com>
2021-08-15 16:41:13 +0800

    feat: add an example

--- Polyfill/myTrim.js
+++ Polyfill/myTrim.js
@@ -1,7 +1,11 @@
 String.prototype.myTrim = function() {
-  var str = this = str.replace(/^\s\s*/, ''),
+  var str = this,
+  str = str.replace(/^\s\s*/, ''),
   whiteSpace = /\s/,
   len = str.length
-  while(whiteSpace.test(str.charAt(--i)))
-  return str.slice(0, i + 1)
+  while(whiteSpace.test(str.charAt(--len)))
+  return str.slice(0, len + 1)
 }
+
+var str = '   test string   '
+console.log(str.myTrim())
